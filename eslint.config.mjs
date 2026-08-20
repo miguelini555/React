0db0950e5a1bf5247e8eda93b1a5105eb7638fd3
miguelini555 <guevarasarmientoemanuel4@gmail.com>
2026-08-19 21:34:28 -0400

    Correcciones paquetes

---
+++ eslint.config.mjs
@@ -0,0 +1,11 @@
+import js from "@eslint/js";
+import globals from "globals";
+export default [
+{
+files: ["**/*.js"],
+languageOptions: {
+globals: globals.browser
+}
+},
+js.configs.recommended
+];
