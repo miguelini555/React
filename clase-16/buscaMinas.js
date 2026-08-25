d83f22e8529fd4144b2fb682c21d01db2c34b375
miguelini555 <guevarasarmientoemanuel4@gmail.com>
2026-08-25 18:22:35 -0400

    Avance clase 25/08

---
+++ clase-16/buscaMinas.js
@@ -0,0 +1,28 @@
+"use strict";
+console.log("Esto es un buscaminas");
+let tabla = document.querySelector(".buscaminas");
+if (tabla) {
+    console.log(tabla.tagName);
+    console.log(tabla.classList);
+}
+let celda = document.querySelector("td");
+if (celda) {
+    console.log(celda.textContent);
+}
+else {
+    console.log("No existe");
+}
+let listaElementos = document.querySelectorAll("td");
+for (const elemento of listaElementos) {
+    elemento.addEventListener("click", function () {
+        console.log(elemento.textContent);
+        console.log(elemento.classList.value);
+        if (elemento.classList.contains("mina")) {
+            for (const item of listaElementos) {
+                item.textContent = "🔥";
+                item.style.color = "gray";
+            }
+        }
+        elemento.style.color = "gray ";
+    });
+}
