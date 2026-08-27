ea6b003a5d96069ac2735a0f7612e93f09785949
miguelini555 <guevarasarmientoemanuel4@gmail.com>
2026-08-27 17:52:35 -0400

    Avance clase 27/08

--- tres-en-raya/src/TresEnRaya.jsx
+++ tres-en-raya/src/TresEnRaya.jsx
@@ -1,3 +1,17 @@
 export default function TresEnRaya() {
-    return <section>Hola</section>
+    return (
+        <section>
+            <table>
+                <tr>
+                    <td></td><td></td><td></td>
+                </tr>
+                <tr>
+                    <td></td><td></td><td></td>
+                </tr>
+                <tr>
+                    <td></td><td></td><td></td>
+                </tr>
+            </table>
+        </section>
+    )    
 }
