19c6527010058bb4d1898ff67dac96e03ecbf7fa
miguelini555 <guevarasarmientoemanuel4@gmail.com>
2026-09-01 18:25:17 -0400

    config5

--- tres-en-raya/src/App.jsx
+++ tres-en-raya/src/App.jsx
@@ -2,7 +2,6 @@ import './App.css'
 import './TresEnRaya.css'
 import {createRoot} from 'react-dom/client'
 import TresEnRaya from './TresEnRaya'
-import Ejemplollaves from './Ejemplollaves'
 //import Contador from './Contador'
 const contenedor = document.querySelector("#root")
 const root = createRoot(contenedor)
